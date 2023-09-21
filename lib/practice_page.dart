6df87e94475bc5a2b2d27ca920442abcc1e8c0ba
MohamedArafath205 <mohamedarafath205@gmail.com>
2023-09-21 19:49:48 +0530

    Day 04 contribution

--- lib/practice_page.dart
+++ lib/practice_page.dart
@@ -6,32 +6,59 @@ class PracticePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Row(
-          children: [
-            InkWell(
-              onTap: () {},
-              child: Container(
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(
-                    color: const Color(0xff00FF00),
-                    borderRadius: BorderRadius.circular(10)),
-                child:
-                    const Icon(Icons.arrow_back, color: Colors.white, size: 20),
+        appBar: AppBar(
+          leading: Row(
+            children: [
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      color: const Color(0xff00FF00),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: const Icon(Icons.arrow_back,
+                      color: Colors.white, size: 20),
+                ),
               ),
-            ),
+            ],
+          ),
+          title: Text("Profile"),
+          centerTitle: true,
+          shadowColor: Colors.black,
+          elevation: 2,
+          actions: [
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.favorite_border)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
           ],
         ),
-        title: Text("Profile"),
-        centerTitle: true,
-        shadowColor: Colors.black,
-        elevation: 2,
-        actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.favorite_border)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-        ],
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Image.asset("../assets/image.jpg"),
+                  Positioned(
+                    left: 40,
+                    child: Column(
+                      children: [
+                        CircleAvatar(
+                          child: Image.asset(
+                            "../assets/img1.png",
+                            width: 100,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const Text("Hello World"),
+            ],
+          ),
+        ));
   }
 }
